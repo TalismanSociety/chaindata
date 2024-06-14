22fd198d7f726e731f01a3e9c2c4f7717327f435
Chid Gilovitz <chid@talisman.xyz>
2024-06-14 13:53:27 +0800

    chore: improved env var handling

--- scripts/shared/fetchFromCoingecko.ts
+++ scripts/shared/fetchFromCoingecko.ts
@@ -4,6 +4,8 @@ import urlJoin from 'url-join'
 import { COINGECKO_API_KEY_NAME, COINGECKO_API_KEY_VALUE, COINGECKO_API_URL } from './constants'
 
 export const fetchFromCoingecko = async (relativeUrl: string, init: RequestInit = {}) => {
+  if (!COINGECKO_API_KEY_VALUE) console.warn('COINGECKO_API_KEY_VALUE is not set')
+
   const headers =
     COINGECKO_API_KEY_NAME && COINGECKO_API_KEY_VALUE
       ? {
@@ -11,6 +13,5 @@ export const fetchFromCoingecko = async (relativeUrl: string, init: RequestInit 
         }
       : {}
   const enhancedInit = merge(init, { headers })
-
   return fetch(urlJoin(COINGECKO_API_URL, relativeUrl), enhancedInit)
 }
