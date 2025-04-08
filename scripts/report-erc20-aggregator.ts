a13bd71ee70de0fecda2c3f287c3ef5bf23fb6b9
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-04-08 17:14:40 +0900

    chore: cleanup

--- scripts/report-erc20-aggregator.ts
+++ scripts/report-erc20-aggregator.ts
@@ -9,15 +9,19 @@ console.log('%d networks', evmNetworks.length)
 
 const results = evmNetworks
   .map((evmNetwork) => {
-    const { id, name, erc20aggregator, balancesConfig } = evmNetwork
+    const { id, name, erc20aggregator, balancesConfig, isDefault, forceScan } = evmNetwork
     const erc20Module = balancesConfig.find((m) => m.moduleType === 'evm-erc20') as any
     const tokens = erc20Module?.moduleConfig?.tokens.length ?? 0
+
+    // if default or forcescan, need an aggregator because of Asset Discovery in the wallet
+    const needed = isDefault || forceScan ? 'YES' : ''
 
     return {
       id,
       name,
       tokens,
       erc20aggregator,
+      needed,
     }
   })
   .filter((a) => a.tokens)
