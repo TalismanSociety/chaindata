2f4d19f69d1c1372ba3361afd0d69c79ed0c6db8
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-04-08 10:36:26 +0900

    chore: ignore tron - fetch external

--- scripts/fetch-external/steps/fetchKnownEvmNetworks.ts
+++ scripts/fetch-external/steps/fetchKnownEvmNetworks.ts
@@ -13,6 +13,7 @@ const IGNORED_CHAINS = [
   1313500, // Xerom, a dead project with malicious RPC url
   1200, // Cuckoo chain - can be un-ignored when https://github.com/MetaMask/eth-phishing-detect/issues/89066 closed
   1210, // Cuckoo testnet - as above
+  728126428, // TRX - unsupported address format
 ]
 
 // RPCs that are not to be fail both from github and browser
