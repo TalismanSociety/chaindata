d5842447f1d80faf31eb7dbc5f9aceaaee8cf706
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-06-24 12:31:27 +0900

    fix: hacked chaindata provider

--- scripts/fetch-external/steps/helpers/getHackedBalanceModuleDeps.ts
+++ scripts/fetch-external/steps/helpers/getHackedBalanceModuleDeps.ts
@@ -10,83 +10,46 @@ import { DotNetworkConfig } from '../../../shared/schemas'
 import { withTimeout } from '../../../shared/util'
 
 export const getHackedBalanceModuleDeps = (chain: DotNetworkConfig, provider: WsProvider) => {
+  const network = chain as unknown as Network
+
   const stubChaindataProvider: IChaindataProvider = {
-    // @ts-ignore
-    chainsObservable: of([chain as unknown as DotNetwork]),
-    chains: () => Promise.resolve([chain as unknown as DotNetwork]),
+    networks$: of([network]),
+    // @ts-expect-error
+    getNetworks$: () => of([network]),
+    // @ts-expect-error
+    getNetworks: () => Promise.resolve([network]),
 
-    networksObservable: of([chain as unknown as Network]),
-    networks: () => Promise.resolve([chain as unknown as Network]),
+    getNetworkIds$: () => of([network.id]),
+    getNetworkIds: () => Promise.resolve([network.id]),
 
-    customNetworksObservable: of([chain as unknown as Network]),
-    customNetworks: () => Promise.resolve([chain as unknown as Network]),
+    // @ts-expect-error
+    getNetworksMapById$: () => of({ [network.id]: network }),
+    // @ts-expect-error
+    getNetworksMapById: () => Promise.resolve({ [network.id]: network }),
 
-    // @ts-ignore
-    customChainsObservable: from(Promise.resolve([])),
-    customChains: () => Promise.resolve([]),
+    // @ts-expect-error
+    getNetworksMapByGenesisHash$: () => of({}),
+    getNetworksMapByGenesisHash: () => Promise.resolve({}),
 
-    // @ts-ignore
-    chainIdsObservable: from(Promise.resolve([chain.id])),
-    chainIds: () => Promise.resolve([chain.id]),
+    getNetworkById$: () => of(network),
+    // @ts-expect-error
+    getNetworkById: () => Promise.resolve(network),
 
-    networkIdsObservable: from(Promise.resolve([chain.id])),
-    networkIds: () => Promise.resolve([chain.id]),
+    getNetworkByGenesisHash$: () => of(null),
+    getNetworkByGenesisHash: () => Promise.resolve(null),
 
-    // @ts-ignore
-    chainsByIdObservable: from(Promise.resolve({ [chain.id]: chain as unknown as DotNetwork })),
-    chainsById: () => Promise.resolve({ [chain.id]: chain as unknown as DotNetwork }),
+    tokens$: of([]),
+    getTokens$: () => of([]),
+    getTokens: () => Promise.resolve([]),
 
-    networksByIdObservable: from(Promise.resolve({ [chain.id]: chain as unknown as Network })),
-    networksById: () => Promise.resolve({ [chain.id]: chain as unknown as Network }),
+    getTokenIds$: () => of([]),
+    getTokenIds: () => Promise.resolve([]),
 
-    // @ts-ignore
-    chainsByGenesisHashObservable: from(Promise.resolve({})),
-    chainsByGenesisHash: () => Promise.resolve({}),
+    getTokensMapById$: () => of({}),
+    getTokensMapById: () => Promise.resolve({}),
 
-    networksByGenesisHashObservable: from(Promise.resolve({})),
-    networksByGenesisHash: () => Promise.resolve({}),
-
-    chainById: (chainId: ChainId) => Promise.resolve(chainId === chain.id ? (chain as unknown as DotNetwork) : null),
-    chainByGenesisHash: (genesisHash: `0x${string}`) => Promise.resolve(null),
-
-    networkById: (chainId: ChainId) => Promise.resolve(chainId === chain.id ? (chain as unknown as Network) : null),
-    networkByGenesisHash: (genesisHash: `0x${string}`) => Promise.resolve(null),
-
-    // @ts-ignore
-    evmNetworksObservable: from(Promise.resolve([])),
-    evmNetworks: () => Promise.resolve([]),
-
-    // @ts-ignore
-    customEvmNetworksObservable: from(Promise.resolve([])),
-    customEvmNetworks: () => Promise.resolve([]),
-
-    // @ts-ignore
-    evmNetworkIdsObservable: from(Promise.resolve([])),
-    evmNetworkIds: () => Promise.resolve([]),
-
-    // @ts-ignore
-    evmNetworksByIdObservable: from(Promise.resolve({})),
-    evmNetworksById: () => Promise.resolve({}),
-
-    evmNetworkById: (evmNetworkId: EvmNetworkId) => Promise.resolve(null),
-
-    // @ts-ignore
-    tokensObservable: from(Promise.resolve([])),
-    tokens: () => Promise.resolve([]),
-
-    // @ts-ignore
-    customTokensObservable: from(Promise.resolve([])),
-    customTokens: () => Promise.resolve([]),
-
-    // @ts-ignore
-    tokenIdsObservable: from(Promise.resolve([])),
-    tokenIds: () => Promise.resolve([]),
-
-    // @ts-ignore
-    tokensByIdObservable: from(Promise.resolve({})),
-    tokensById: () => Promise.resolve({}),
-
-    tokenById: (tokenId: TokenId) => Promise.resolve(null),
+    getTokensById$: () => of({}),
+    getTokensById: () => Promise.resolve({}),
   }
   const stubChainConnector = {
     asProvider(chainId: ChainId): ProviderInterface {
